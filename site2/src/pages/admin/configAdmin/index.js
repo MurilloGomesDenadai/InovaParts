e50be18c649a1336770967f84592eae2f2ef3504
Luiz-Dias01 <142491324+Luiz-Dias01@users.noreply.github.com>
2023-09-20 18:20:06 -0300

    update index

--- site2/src/pages/admin/configAdmin/index.js
+++ site2/src/pages/admin/configAdmin/index.js
@@ -3,31 +3,72 @@ import {Link} from 'react-router-dom'
 import { useState } from 'react';
 import axios from 'axios'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function Config() {
   const [listarTodos, setListarTodos] = useState ([])
   const [nome, setNome] = useState ('')
+  const [produto, setProduto] = useState ('')
+  const [valor, setValor] = useState ('')
+  const [quantidade, setQuantidade] = useState ('')
+  const [disponivel, setDisponivel] = useState (true)
+  const [modelo, setModelo] = useState ('')
+  const [detalhes, setDetalhes] = useState ('')
+  const [promocao, setPromocao] = useState ('')
+  const [categoria, setCategoria] = useState ('')
+  const [marca, setMarca] = useState ('')
+
+
+  async function Adicionar() {
+
+    let url = 'http://localhost:5000/produto';
+    await axios.post(url, {
+      categoria: categoria,
+      nome: produto,
+      marca: marca,
+      modelo: modelo,
+      disponivel: disponivel,
+      promocao: promocao,
+      valor: valor,
+      detalhes: detalhes,
+      quantidade: quantidade
+    })
+
+    toast.success("Registro Salvo!")
+  }
 
   async function Listar() {
     
-    if (setNome == '') {
-      let url = 'http://localhost:5000/produto';
-      let resp = await axios.get(url)
+    let url = 'http://localhost:5000/produto';
+    let resp = await axios.get(url)
 
-      setListarTodos(resp.data)
-    } else {
-      let url = `http://localhost:5000/produto/busca?nome= ${nome}`
-      let resp = await axios.get(url)
-
-      setListarTodos(resp.data)
-    }
-    
-    
-
-    
-    
-    
-    
+    setListarTodos(resp.data)
   }
+
+  async function Apagar(id) {
+    let res = await axios.delete(`http://localhost:5000/produto/ ${id}`)
+    
+    toast.success("Registro Deletado!")
+    
+    let url = 'http://localhost:5000/produto';
+    let resp = await axios.get(url)
+
+    setListarTodos(resp.data)
+  }
+
+  async function Alterar(item) {
+      setCategoria(item.categoria)
+      setNome(item.nome)
+      setMarca(item.marca)
+      setModelo(item.modelo)
+      setDisponivel(item.disponivel)
+      setPromocao(item.promocao)
+      setValor(item.valor)
+      setDetalhes(item.detalhes)
+      setQuantidade(item.quantidade)
+  }
+
 
   return (
     <div className="pagina-config">
@@ -92,27 +133,23 @@ export default function Config() {
                 <div>
                   <div className='caixa-inserir'>
                     <label>
-                      Peça
+                      Produto
                     </label>
-                    <input type='text' />
+                    <input type='text' value={produto} onChange={e => setProduto(e.target.value)}/>
                   </div>
 
                   <div className='caixa-inserir'>
                     <label>
                       Valor
                     </label>
-                    <input type='text' />
+                    <input type='text' value={valor} onChange={e => setValor(e.target.value)}/>
                   </div>
 
                   <div className='caixa-inserir'>
                     <label>
-                      Marca
+                      Categoria
                     </label>
-                    <select name="Marca" id="Marca">
-                      <option value="Select">Selecione</option>
-                      <option value="Carro">Celta</option>
-                      <option value="Moto">Gol</option>
-                    </select>
+                    <input type='text' value={categoria} onChange={e => setCategoria(e.target.value)}/>
                   </div>
 
                   <div id='group'>
@@ -120,14 +157,14 @@ export default function Config() {
                       <label>
                         QTD
                       </label>
-                      <input type='number' />
+                      <input type='text' value={quantidade} onChange={e => setQuantidade(e.target.value)}/>
                     </div>
 
                     <div className='caixa-inserir'>
                       <label>
                         Disponivel
                       </label>
-                      <input type='text' />
+                      <input type='checkbox' value={disponivel} onChange={e => setDisponivel(e.target.checked)}/>
                     </div>
                   </div>
 
@@ -135,22 +172,29 @@ export default function Config() {
                     <label>
                       Modelo
                     </label>
-                    <input type='text' />
+                    <input type='text' value={modelo} onChange={e => setModelo(e.target.value)}/>
                   </div>
 
                   <div className='caixa-inserir'>
                     <label>
-                      Promoção
+                      Marca
                     </label>
-                    <input type='text' />
+                    <input type='text' value={marca} onChange={e => setMarca(e.target.value)}/>
                   </div>
                 </div>
 
                 <div className='caixa-descricao'>
+                    <label>
+                      Promoção
+                    </label>
+                    <input type='text' value={promocao} onChange={e => setPromocao(e.target.value)}/>
+                </div>
+
+                <div className='caixa-descricao'>
                   <label>
-                    Descrição
+                    Detalhes
                   </label>
-                  <input type='text' />
+                  <input type='text' value={detalhes} onChange={e => setDetalhes(e.target.value)}/>
                 </div>
               </form>
 
@@ -187,7 +231,7 @@ export default function Config() {
                 </div>
 
                 <div id='btn-salvar'>
-                  <button>Salvar</button>
+                  <button onClick={Adicionar}>Salvar</button>
                 </div>
               </div>
             </div>
@@ -196,55 +240,62 @@ export default function Config() {
           </div>
 
           <div id='consulta'>
-            <div>
+            <div id='conjunto'>
               <div id='enunciado-consulta'>
                 <h2>Lista de produtos</h2>
               </div>
+
               <div className='caixa-consulta'>
                   <div>
                     <label>Nome do produto</label>
                     <input type='text' onChange={e => setNome(e.target.value)}value={nome}/>
                   </div>
+
                   <div id='img-consulta'>
                     <button onClick={Listar}><img src='../../assets/icon/lupa.png'/></button>
                   </div>
                 </div>
             </div>
-              <table>
-                <thead>
-                  <tr>
-                    <th>#ID</th>
-                    <th>Peça</th>
-                    <th>Marca</th>
-                    <th>Disponível</th>
-                    <th>QTD</th>
-                    <th>Promoção</th>
-                    <th>Valor</th>
-                    <th>Promoção</th>
-                  </tr>
-                </thead>
 
-                <tbody>
-                    {listarTodos.map(item =>
-                      <tr>
-                        <td>{item.Id}</td>
-                        <td>{item.Produto}</td>
-                        <td>{item.Marca}</td>
-                        <td>{item.Disponivel}</td>
-                        <td>{item.Quantidade}</td>
-                        <td>{item.Promocao}</td>
-                        <td>{item.Valor}</td>
-                        <td>
-                          <div className='interacao'>
-                            <div><img src='../../assets/icon/alterar.png' /></div>
-                            <div><img src='../../assets/icon/lixeira.png' /></div>
-                          </div>
-                        </td>
-                      </tr>           
-                  )}
-                  
-                </tbody>
-              </table>
+              <div id='scroll-horizontal'>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>#ID</th>
+                      <th>Peça</th>
+                      <th>Marca</th>
+                      <th>Disponível</th>
+                      <th>QTD</th>
+                      <th>Promoção</th>
+                      <th>Valor</th>
+                      <th>Promoção</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      {listarTodos.map(item =>
+                        <tr>
+                          <td className='td-center'>{item.Id}</td>
+                          <td>{item.Produto}</td>
+                          <td>{item.Marca}</td>
+                          <td>{item.Disponivel}</td>
+                          <td className='td-center'>{item.Quantidade}</td>
+                          <td>{item.Promocao}</td>
+                          <td>{item.Valor}</td>
+                          <td>
+                            <div className='interacao'>
+                              <div><button onClick={() => Alterar(item)}><img src='../../assets/icon/alterar.png' /></button></div>
+                              <div><button onClick={() => Apagar(item.Id)}><img src='../../assets/icon/lixeira.png' /></button></div>
+                            </div>
+                          </td>
+                        </tr>
+                    )}
+                
+                  </tbody>
+                </table>
+              </div>
+            <div>
+              <ToastContainer />
+            </div>
           </div>
         </main>
       </div>
