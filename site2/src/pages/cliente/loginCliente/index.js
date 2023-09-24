98022d280555636e37793844b1c54638458aaf6b
Maria Alice <72289711+The-Ongaro@users.noreply.github.com>
2023-09-23 23:52:27 -0300

    loginCliente

--- site2/src/pages/cliente/loginCliente/index.js
+++ site2/src/pages/cliente/loginCliente/index.js
@@ -1,8 +1,32 @@
 import './index.scss';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState('');
+  
+  const navigate = useNavigate();
+
+  async function enterClick() {
+      try {
+        const resposta = await axios.post('http://localhost:5000/usuario/login', {
+          email: email,
+          senha: senha
+        });
+
+        navigate('/telaCompra');
+
+      } catch (err) {
+        if(err.response.status === 401){
+          setErro(err.response.data.erro);
+        }
+      }
+  }
+
   return (
     <div id='pagina-login'>
       <header>
@@ -18,17 +42,19 @@ export default function Login() {
           <form>
             <div className='caixa-senha'>
               <label>Digite seu Email</label>
-              <input type='text' placeholder='Email'/>
+              <input type='text' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
             </div>
 
             <div className='caixa-senha'>
               <label>Digite sua Senha</label>
-              <input type='text' placeholder='Senha'/>
+              <input type='password' placeholder='Senha' value={senha} onChange={e => setSenha(e.target.value)}/>
             </div>
           </form>
+
+          <div>{erro}</div>
           
           <div id='btn-entrar'>
-            <Link to='/telaCompra'><button>Entrar</button></Link>
+          <button onClick={enterClick}>Entrar</button>
           </div>
     
           <div id ='nav-cadastro'>
@@ -37,7 +63,5 @@ export default function Login() {
         </main>
       </div>
     </div>
-  );
+  )
 }
-
-
