bd52d40477d98077f7691f79f575a31be9f67d13
Maria Alice <72289711+The-Ongaro@users.noreply.github.com>
2023-09-26 08:44:20 -0300

    Update index.js

--- site2/src/pages/admin/loginAdmin/index.js
+++ site2/src/pages/admin/loginAdmin/index.js
@@ -1,8 +1,32 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
+
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function Admin () {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState('');
+
+    const navigate = useNavigate();
+
+    async function entraClick() {
+        try {
+            const resposta = await axios.post('http://localhost:5000/adm/login', {
+                email: email,
+                senha: senha
+            });
+    
+            navigate('/configAdm');
+
+        } catch (err) {
+            if(err.response.status === 401) {
+                setErro(err.response.data.erro);
+            }
+        }
+    }
+
     return (
         <div id='pagina-admin'>
             <header>
@@ -18,12 +42,12 @@ export default function Admin () {
                     <form>
                         <div className='caixa-senha'>
                             <label>Digite seu Email</label>
-                            <input type='text' placeholder='Email'/>
+                            <input type='text' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
                         </div>
 
                         <div className='caixa-senha'>
                             <label>Digite sua Senha</label>
-                            <input type='text' placeholder='Senha'/>
+                            <input type='password' placeholder='Senha' value={senha} onChange={e => setSenha(e.target.value)}/>
                         </div>
 
                         <div id='senha'>
@@ -31,8 +55,10 @@ export default function Admin () {
                         </div>
                     </form>
 
+                    <div>{erro}</div>
+
                     <div id='btn-entrar'>
-                        <Link to='/telaCompra'><button>Entrar</button></Link>
+                        <button onClick={entraClick}>Entrar</button>
                     </div>
 
                     <div id ='nav-cadastro'>
