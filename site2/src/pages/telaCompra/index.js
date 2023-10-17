f1279e79c426bc22bb8b773974485178d23d5220
Luiz-Dias01 <142491324+Luiz-Dias01@users.noreply.github.com>
2023-10-17 11:22:47 -0300

    manutenção

--- site2/src/pages/telaCompra/index.js
+++ site2/src/pages/telaCompra/index.js
@@ -6,36 +6,42 @@ export default function Compra() {
     return (
         <div id='pagina-compra'>
             <header>
-              <nav>
-                <div id='menu'>
-                    <div>
-                        <img src='../assets/icon/logo.png'/>
+                <nav>
+                    <div id='menu'>
+                        <div>
+                            <img src='../assets/icon/logo.png'/>
+                        </div>
+
+                        <div>
+                            <img src='../assets/icon/icone_Usuario.png'/>
+                        </div>
+                    </div>
+                </nav>
+
+                <div id='area-pesquisa'>
+                    <div className='txt-header'>
+                        <h1>Inovapartes</h1>
+                        <p>
+                            Invista no seu veículo  
+                        </p>
+                        <p>
+                            do conforto e com a segurança da sua <span>casa!</span>
+                        </p>
                     </div>
 
-                    <div>
-                        <img src='../assets/icon/icone_Usuario.png'/>
+                    <div id='alinhar-caixa-pesquisa'>
+                        <div id='caixa-pesquisa'>
+                            <input type='text' placeholder='Pesquisa'/>
+                            <img src='../assets/icon/lupa.png'/>
+                        </div>
                     </div>
                 </div>
-              </nav>
-
-              <div id='area-pesquisa'>
-                <div className='txt-header'>
-                    <h1>Inovapartes</h1>
-                    <p>
-                        Invista no seu veículo do conforto e com a segurança da sua <span>casa!</span>
-                    </p>
-                </div>
-
-                <div>
-                    <input type='text'><img src='../assets/icon/lupa.png'/></input>
-                    
-                        
-                </div>
-              </div>
             </header>
 
-            <main
-            >
+            <main>
+                <section>
+                    
+                </section>
 
             </main>
 
