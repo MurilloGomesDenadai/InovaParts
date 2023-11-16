17aa205b39d399b2c9efcddc772d03e8073451bf
Luiz_Henrique <142491324+Luiz-Dias01@users.noreply.github.com>
2023-11-16 11:02:06 -0300

    Update clienteEndpoints.js

---
+++ site2/src/api/clienteEndpoints.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+const api = axios.create({
+    baseURL: 'http://localhost:5011'
+});
+//Login
+export async function loginCliente(email, senha) {
+    const resp = await api.post('/usuario/login', {
+        email: email,
+        senha: senha
+    })
+    return resp.data
+};
+//listar clientes
+export async function listarCliente() {
+    const resp = await api.get('/usuario')
+    return resp.data
+};
+//Listar clientes por Nome
+export async function listarporNome(nome) {
+    const resp = await api.get(`/usuario/busca?nome=${nome}`)
+    return resp.data
+};
