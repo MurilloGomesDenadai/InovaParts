253d446d1ecc8fa82016df9b07746c57678b56dd
MurilloGomesDenadai <130002325+MurilloGomesDenadai@users.noreply.github.com>
2023-11-24 08:28:28 -0300

    rotas.

--- site2/src/api/constants.js
+++ site2/src/api/constants.js
@@ -1,7 +1,7 @@
 
 
 
-export const API_URL = 'http://localhost:5011';
-//export const API_URL = 'http://129.148.42.252:5011';
+// export const API_URL = 'http://localhost:5011';
+export const API_URL = 'http://129.148.42.252:5011';
 
 
